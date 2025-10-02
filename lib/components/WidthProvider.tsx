68bdef2889225023f8eab45348317aeb0fa08217
Dima <9010963+message-dimke@users.noreply.github.com>
2025-10-02 21:59:47 +0300

    Convert WidthProvider from Flow to TypeScript

--- lib/components/WidthProvider.tsx
+++ lib/components/WidthProvider.tsx
@@ -1,55 +1,42 @@
-// @flow
 import * as React from "react";
 import PropTypes from "prop-types";
 import ResizeObserver from "resize-observer-polyfill";
 import clsx from "clsx";
 import type { ReactRef } from "../ReactGridLayoutPropTypes";
 
-type WPDefaultProps = {|
-  measureBeforeMount: boolean
-|};
+// TypeScript conversion from Flow
 
-// eslint-disable-next-line no-unused-vars
-type WPProps = {|
-  className?: string,
-  style?: Object,
-  ...WPDefaultProps
-|};
+type WPDefaultProps = {
+  measureBeforeMount: boolean;
+};
 
-type WPState = {|
-  width: number
-|};
+type WPProps = {
+  className?: string;
+  style?: React.CSSProperties;
+} & WPDefaultProps;
 
-type ComposedProps<Config> = {|
-  ...Config,
-  measureBeforeMount?: boolean,
-  className?: string,
-  style?: Object,
-  width?: number
-|};
+type WPState = {
+  width: number;
+};
+
+type ComposedProps<Config> = Config & {
+  measureBeforeMount?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  width?: number;
+};
 
 const layoutClassName = "react-grid-layout";
 
-/*
- * A simple HOC that provides facility for listening to container resizes.
- *
- * The Flow type is pretty janky here. I can't just spread `WPProps` into this returned object - I wish I could - but it triggers
- * a flow bug of some sort that causes it to stop typechecking.
- */
-export default function WidthProvideRGL<Config>(
-  ComposedComponent: React.AbstractComponent<Config>
-): React.AbstractComponent<ComposedProps<Config>> {
-  return class WidthProvider extends React.Component<
-    ComposedProps<Config>,
-    WPState
-  > {
+export default function WidthProvideRGL<Config extends {}>(
+  ComposedComponent: React.ComponentType<Config & { innerRef: React.RefObject<HTMLDivElement>; width: number }>
+): React.ComponentType<ComposedProps<Config>> {
+  return class WidthProvider extends React.Component<WPProps, WPState> {
     static defaultProps: WPDefaultProps = {
       measureBeforeMount: false
     };
 
     static propTypes = {
-      // If true, will not render children until mounted. Useful for getting the exact width before
-      // rendering, to prevent any unsightly resizing.
       measureBeforeMount: PropTypes.bool
     };
 
@@ -57,9 +44,9 @@ export default function WidthProvideRGL<Config>(
       width: 1280
     };
 
-    elementRef: ReactRef<HTMLDivElement> = React.createRef();
+    elementRef: React.RefObject<HTMLDivElement> = React.createRef();
     mounted: boolean = false;
-    resizeObserver: ResizeObserver;
+    resizeObserver!: ResizeObserver;
 
     componentDidMount() {
       this.mounted = true;
@@ -92,7 +79,6 @@ export default function WidthProvideRGL<Config>(
           <div
             className={clsx(this.props.className, layoutClassName)}
             style={this.props.style}
-            // $FlowIgnore ref types
             ref={this.elementRef}
           />
         );
@@ -101,7 +87,7 @@ export default function WidthProvideRGL<Config>(
       return (
         <ComposedComponent
           innerRef={this.elementRef}
-          {...rest}
+          {...(rest as Config)}
           {...this.state}
         />
       );
